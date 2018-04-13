43f8c80e6667af9cb8acbae7aed396d816dc1558
Angel Jasso <jjassobarba@gmail.com>
2018-04-12 22:52:42 -0500

    Backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'seedany';
+
+// Google
+module.exports.CLIENT_ID = '806795688503-ih0cmm8rgj5t8ehn4rjs98lui2geadcg.apps.googleusercontent.com';
+;module.exports.GOOGLE_SECRET = '8q-I7Saq68k4cM9XcXHpzt0v'
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+const {
+    OAuth2Client
+} = require('google-auth-library');
 
 var SEED = require('../config/config').SEED;
 
@@ -8,11 +11,107 @@ var app = express();
 
 var Usuario = require('../models/usuario');
 
+// Google
+const CLIENT_ID = require('../config/config').CLIENT_ID;
+const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
+const client = new OAuth2Client(CLIENT_ID);
+
+// Autenticacion google
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+app.post('/google', async (req, res) => {
+    var token = req.body.token;
+
+    var googleUser = await verify(token)
+                    .catch(e => {
+                        return res.status(403).json({
+                            ok: false,
+                            mensaje: 'Token invalido'
+                        });
+                    });
+
+    Usuario.findOne({email: googleUser.email}, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuarios',
+                errors: err
+            });
+        }
+
+        if (usuarioDB) {
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe usar autenticacion con la que se registró',
+                });
+            } else {
+                var token = jwt.sign({
+                    usuario: usuarioBD
+                }, SEED, {
+                    expiresIn: 14400
+                });
+        
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioBD,
+                    token: token,
+                    id: usuarioBD._id
+                });
+            }
+        } else {
+            var usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true,
+            usuario.password = ':)';
+
+            usuario.save((err, usuarioDB) => {
+                var token = jwt.sign({
+                    usuario: usuarioBD
+                }, SEED, {
+                    expiresIn: 14400
+                });
+        
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioBD,
+                    token: token,
+                    id: usuarioBD._id
+                });
+            });
+        }
+    });
+});
+
+// Autenticacion normal
 app.post('/', (req, res) => {
-    
+
     var body = req.body;
 
-    Usuario.findOne({email: body.email}, (err, usuarioBD) => {
+    Usuario.findOne({
+        email: body.email
+    }, (err, usuarioBD) => {
 
         if (err) {
             return res.status(500).json({
@@ -30,7 +129,7 @@ app.post('/', (req, res) => {
             });
         }
 
-        if(!bcrypt.compareSync(body.password, usuarioBD.password)){
+        if (!bcrypt.compareSync(body.password, usuarioBD.password)) {
             return res.status(400).json({
                 ok: false,
                 mensaje: "Credenciales incorrectas - password",
@@ -40,7 +139,11 @@ app.post('/', (req, res) => {
 
         // Crear token
         usuarioBD.password = ':)';
-        var token = jwt.sign({usuario: usuarioBD}, SEED, {expiresIn: 14400});
+        var token = jwt.sign({
+            usuario: usuarioBD
+        }, SEED, {
+            expiresIn: 14400
+        });
 
         res.status(200).json({
             ok: true,
@@ -49,8 +152,10 @@ app.post('/', (req, res) => {
             id: usuarioBD.id
         });
     });
-    
-   
+
+
 });
+
+
 
 module.exports = app;
