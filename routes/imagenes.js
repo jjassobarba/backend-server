1ba20454413220de08ddb83201714e469e42747e
Angel Jasso <jjassobarba@gmail.com>
2018-04-20 09:57:14 -0500

    Agregado path para regresar imagenes en node

--- routes/imagenes.js
+++ routes/imagenes.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var fs = require('fs');
 
+const path = require('path');
+
 var app = express();
 
 // Rutas
@@ -9,18 +11,14 @@ app.get('/:tipo/:img', (req, res, next) => {
     var tipo = req.params.tipo;
     var img = req.params.img;
 
-    var path = `./uploads/${ tipo }/${ img }`;
+    var pathImagen = path.resolve(__dirname, `../uploads/${ tipo }/${ img }`);
 
-    fs.exists(path, existe => {
-
-        if(!existe){
-            path = './assets/no-img.jpg';
-        }
-
-        res.sendFile(path);
-
-
-    })   
+    if (fs.existsSync(pathImagen)) {
+        res.sendFile(pathImagen);
+    } else {
+        var pathNoImagen = path.resolve(__dirname, '../assets/no-img.jpg');
+        res.sendFile(pathNoImagen);
+    }
 });
 
 module.exports = app;
